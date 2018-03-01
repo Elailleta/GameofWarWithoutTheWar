4bba8789297968977cb573b5ca162cc6e8067c1f
Elailleta <31962039+Elailleta@users.noreply.github.com>
2018-02-28 19:49:59 -0500

    Add files via upload

---
+++ Game of war (without the war)/src/GameOfWar.java
@@ -0,0 +1,234 @@
+import java.util.*;
+
+public class GameOfWar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		//I MISSNAMED IT IM SORRY 
+
+		//make the ints
+		int num1;
+		int num2;
+		int num3;
+		int num4;
+		int num5;
+		int num6;
+		int num7;
+		int num8;
+		int num9;
+		int num10;
+		int temp;
+		Scanner in=new Scanner(System.in);
+
+		//ASK THE USER FOR 10 NUMBERS
+		System.out.println("Please choose ten different numbers");
+		num1=in.nextInt();
+		num2=in.nextInt();
+		num3=in.nextInt();
+		num4=in.nextInt();
+		num5=in.nextInt();
+		num6=in.nextInt();
+		num7=in.nextInt();
+		num8=in.nextInt();
+		num9=in.nextInt();
+		num10=in.nextInt();
+
+		//ORDER THE NUMBERS FROM LOWEST TO HIGHEST
+		//IF 1>2, 1 SWITCH 2
+		//if 1>2 STAY THE SAME
+
+		if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+		}if (num5>num6){
+			temp=num5;
+			num5=num6;
+			num6=temp;
+		}if (num6>num7){
+			temp=num6;
+			num6=num7;
+			num7=temp;
+		}if (num7>num8){
+			temp=num7;
+			num7=num8;
+			num8=temp;
+		}if (num8>num9){
+			temp=num8;
+			num8=num9;
+			num9=temp;
+		}if (num9>num10){
+			temp=num9;
+			num9=num10;
+			num10=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+		}if (num5>num6){
+			temp=num5;
+			num5=num6;
+			num6=temp;
+		}if (num6>num7){
+			temp=num6;
+			num6=num7;
+			num7=temp;
+		}if (num7>num8){
+			temp=num7;
+			num7=num8;
+			num8=temp;
+		}if (num8>num9){
+			temp=num8;
+			num8=num9;
+			num9=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+		}if (num5>num6){
+			temp=num5;
+			num5=num6;
+			num6=temp;
+		}if (num6>num7){
+			temp=num6;
+			num6=num7;
+			num7=temp;
+		}if (num7>num8){
+			temp=num7;
+			num7=num8;
+			num8=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+		}if (num5>num6){
+			temp=num5;
+			num5=num6;
+			num6=temp;
+		}if (num6>num7){
+			temp=num6;
+			num6=num7;
+			num7=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+		}if (num5>num6){
+			temp=num5;
+			num5=num6;
+			num6=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+		}if (num4>num5){
+			temp=num4;
+			num4=num5;
+			num5=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+		}if (num3>num4){
+			temp=num3;
+			num3=num4;
+			num4=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+		}if (num2>num3){
+			temp=num2;
+			num2=num3;
+			num3=temp;
+
+		}if (num1>num2){
+			temp=num1;
+			num1=num2;
+			num2=temp;
+
+		}
+		System.out.println(num1+" "+num2+" "+num3+" "+num4+" "+num5+" "+num6+" "+num7+" "+num8+" "+num9+" "+num10);
+	}
+}
